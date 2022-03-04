a49856416cc39e1794d87d2a1a3512a5fec37b46
kledde0909 <82025597+kledde0909@users.noreply.github.com>
2022-03-04 09:29:30 +0800

    bg-modal and on going vehicle reservation

--- fleet_addvehicle.php
+++ fleet_addvehicle.php
@@ -66,83 +66,78 @@
             <div class="form-group">
                 <label for="v_category">Category</label>
                 <select class="form-control" name="v_category" placeholder="v_category">
-                  <option <?php if('v_category')  echo 'selected="selected"';?>value="3">ARMORED VEHICLE</option>
-                  <option <?php if('v_category')  echo 'selected="selected"';?>value="2">VAN</option>
-                  <option <?php if('v_category')  echo 'selected="selected"';?>value="1">CAR</option>
+                  <option <?php echo 'selected="selected"';?>>Choose Category</option>
+                  <option <?php if('v_category');?>value="3">ARMORED VEHICLE</option>
+                  <option <?php if('v_category');?>value="2">VAN</option>
+                  <option <?php if('v_category');?>value="1">CAR</option>
                 </select>
             </div>
             <div class="form-group">
                 <label for="v_model">Model</label>
-                <input type="text" class="form-control" name ="v_model"  placeholder="v_model">
+                <input type="text" class="form-control" name ="v_model"  placeholder="Car Model">
             </div>
             <div class="form-group">
                 <label for="v_year">Year</label>
-                <input type="text" class="form-control" name ="v_year"  placeholder="v_year">
+                <input type="text" class="form-control" name ="v_year"  placeholder="Year Purchased">
             </div>
             <div class="form-group">
                 <label for="v_color">Color</label>
-                <input type="text" class="form-control" name ="v_color"  placeholder="v_color">
+                <input type="text" class="form-control" name ="v_color"  placeholder="Car Color">
             </div>
             <div class="form-group">
                 <label for="v_regnum">Registration Number</label>
-                <input type="text" class="form-control" name ="v_regnum"  placeholder="v_regnum">
+                <input type="text" class="form-control" name ="v_regnum"  placeholder="Registration Number">
             </div>
             <div class="form-group">
                 <label for="v_serialnum">Serial Number</label>
-                <input type="text" class="form-control" name ="v_serialnum"  placeholder="v_serialnum">
+                <input type="text" class="form-control" name ="v_serialnum"  placeholder="Serial Number">
             </div>
             <div class="form-group">
                 <label for="v_capacity">Maximum Capacity</label>
-                <input type="text" class="form-control" name ="v_capacity"  placeholder="v_capacity">
+                <input type="text" class="form-control" name ="v_capacity"  placeholder="Car Capacity">
             </div>
             <div class="form-group">
                 <label for="v_datepur">Date of Purchase</label>
-                <input type="text" class="form-control" name ="v_datepur"  placeholder="v_datepur">
+                <input type="text" class="form-control" name ="v_datepur"  placeholder="Date Purchased">
             </div>
             <div class="form-group">
                 <label for="v_manu">Manufacturer</label>
-                <input type="text" class="form-control" name ="v_manu"  placeholder="v_manu">
+                <input type="text" class="form-control" name ="v_manu"  placeholder="Manufacturer">
             </div>
             <div class="form-group">
                 <label for="v_enginetype">Engine Type</label>
-                <input type="text" class="form-control" name ="v_enginetype"  placeholder="v_enginetype">
+                <input type="text" class="form-control" name ="v_enginetype"  placeholder="Engine Type">
             </div>
             <div class="form-group">
                 <label for="v_loc">Location of purchase</label>
-                <input type="text" class="form-control" name ="v_loc"  placeholder="v_loc">
+                <input type="text" class="form-control" name ="v_loc"  placeholder="Location of purchase">
             </div>
             <div class="form-group">
                 <label for="v_fueltype">Fuel Type</label>
-                <input type="text" class="form-control" name ="v_fueltype"  placeholder="v_fueltype">
+                <input type="text" class="form-control" name ="v_fueltype"  placeholder="Fuel Type">
             </div>
             <div class="form-group">
                 <label for="v_fuelcap">Fuel Capacity</label>
-                <input type="text" class="form-control" name ="v_fuelcap"  placeholder="v_fuelcap">
+                <input type="text" class="form-control" name ="v_fuelcap"  placeholder="Fuel Capacity">
             </div>
             <div class="form-group">
                 <label for="v_license">License</label>
-                <input type="text" class="form-control" name ="v_license"  placeholder="v_license">
+                <input type="text" class="form-control" name ="v_license"  placeholder="License">
             </div>
             <div class="form-group">
                 <label for="v_condition">Condition</label>
-                <input type="text" class="form-control" name ="v_condition"  placeholder="v_condition">
+                <input type="text" class="form-control" name ="v_condition"  placeholder="Car Condition">
             </div>
             <div class="form-group">
                 <label for="v_avail">Availability</label>
-                <input type="text" class="form-control" name ="v_avail"  placeholder="v_avail">
+                <input type="text" class="form-control" name ="v_avail"  placeholder="Availability">
             </div>
             <div class="form-group clearfix">
               <button type="submit" name="fleet_addvehicle" class="btn btn-success">Submit</button>              
-            </div>
-            
-        </form>
-        
-        <a href="admin.php"><button type="submit" name="fleet_addvehicle" class="btn btn-danger">Home</button></a>
-        <a href="applicant_edit.php"><button type="submit" name="fleet_addvehicle" class="btn btn-success">edit submittion</button></a>
+            </div>            
+          </form>
         </div>
-
       </div>
-
     </div>
   </div>
 
--- vehicles.php
+++ vehicles.php
@@ -23,7 +23,41 @@
           <span>Vehicle Reservation</span>
        </strong>
       </div>
-
+      <div class="col-md-3">  
+            <input type="text" name="from_date" id="from_date" class="form-control" placeholder="From Date" />  
+      </div>  
+      <div class="col-md-3">  
+            <input type="text" name="to_date" id="to_date" class="form-control" placeholder="To Date" />  
+      </div>  
+      <div class="panel-body" id="documentTrackingTable">
+      <table class="table table-bordered table-striped" id="myTable">
+        <thead>
+          <tr>
+            <th class="text-center" style="width: 15%;">Date Receive</th>
+            <th>Request By</th>
+            <th class="text-center" style="width: 15%;">Document Date</th>
+            <th>Document Subject</th>
+            <th>Document Sender</th>
+            <th class="text-center" style="width: 15%;">Application Date</th>
+            <th>Application Remarks</th>
+            <th class="text-center" style="width: 15%;">Reference Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach($data as $a_vendor): ?>
+            <tr>
+            <td><?php echo str_replace('.php', '', (ucwords($a_vendor['module'])))?></td>
+            <td><?php echo remove_junk(ucwords($a_vendor['action_taken']))?></td>
+            <td><?php echo remove_junk(ucwords($a_vendor['name']))?></td>
+            <td><?php echo remove_junk(ucwords($a_vendor['datetime_created']))?></td>
+            <td><?php echo remove_junk(ucwords($a_vendor['module']))?></td>
+            <td><?php echo remove_junk(ucwords($a_vendor['action_taken']))?></td>
+            <td><?php echo remove_junk(ucwords($a_vendor['name']))?></td>
+            <td><?php echo remove_junk(ucwords($a_vendor['datetime_created']))?></td>
+          <?php endforeach;?>
+       </tbody>
+     </table>
+     </div>
       
     </div>
   </div>
